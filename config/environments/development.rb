7a9fd4b98d7f6349634594e73e24ef8e17a90f28
Ubuntu <ubuntu@ip-172-31-61-228.ec2.internal>
2024-10-02 07:54:44 +0000

    Gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "af828f6c11f1415a9b8de96a0814249b.vfs.cloud9.us-east-1.amazonaws.com"
 end
